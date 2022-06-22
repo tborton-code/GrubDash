1ad8aac4838d3bb9c1da5bd097e326a88c89713a
Tyler Borton <tylercborton@gmail.com>
2022-06-22 16:31:14 -0400

    cleaning up

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,7 +6,6 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
 const list = (req, res, next) => {
     res.json({data: dishes});
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
 router
     .route("/:dishId")
     .get(controller.read)
